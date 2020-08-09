8ee323c853bf5def3e179ad8d380d41fcfd3d9da
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-09 20:05:44 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -17,3 +17,22 @@ if
   else puts "Odd!"
 
 end
+
+
+
+
+
+
+
+
+
+
+# current_time = Time.now
+# current_time = current_time.to_i
+ 
+# if current_time.even?
+#   puts "Even!"
+# else
+#   puts "Odd!"
+# end
+
