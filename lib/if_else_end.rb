18eadff3b4fa93279ab4e76d8821029a7db48cec
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-09 20:03:25 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,13 +2,13 @@
 
 
 current_time = Time.now
-
-
-if true
-  puts "Even!"
-else
-  puts "Odd!"
-end
-
-
 current_time = current_time.to_i
+ 
+ 
+#if current_time % 2 == 0
+ 
+     puts "Even!"
+
+else puts "Odd!"
+
+end
